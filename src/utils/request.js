68a00851bc0397ef530b06def2bb16eac846a32f
wb-hyq283934 <wb-hyq283934@antfin.com>
2017-06-15 13:45:43 +0800

    req changes

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,138 +1,38 @@
-import ajax from '@alipay/ajax';
-import {
-  toast,
-  debug,
-  localStorageDebug,
-} from '../utils';
-import {
-  REQUEST_TIMEOUT,
-  ERROR_CODE,
-} from '../configs';
+import fetch from 'dva/fetch';
 
-export function checkLoginStatus(response) {
-  if (response && response.stat === 'deny') {
-    toast({
-      type: 'error',
-      msg: '登录超时，请重新登录！',
-    });
 
-    // 如果没有则跳转到登录地址 response.target
-    /*setTimeout(() => {
-     window.location = '/';
-     }, 500);
-     */
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
   }
-}
 
-// 错误状态处理
-// TODO: 待确定生活号后台错误信息
-export function failedHandler(response = {}) {
-  const {
-    stat, // 状态
-    errorCode, // 错误码
-  } = response;
-  let {
-    errorMessage, // 错误信息
-  } = response;
-  let messageType = 'warning';
-  const isFailed = stat !== 'ok';
-
-  if (isFailed) {
-    /*
-    if (errorCode === ERROR_CODE.NO_PERMISSION) {
-      messageType = 'error';
-      resMsg = errorMessage || '没有操作权限！';
-    } else {
-      resMsg = errorMessage || '请求出错误，请重试！';
-    }
-    */
-
-    errorMessage = errorMessage || '发生错误，请重试！';
-
-    toast({
-      type: messageType,
-      msg: errorMessage,
-    });
-
-    return {
-      status: 'failed',
-      message: errorMessage,
-      code: errorCode,
-    };
-  }
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
 }
 
 /**
  * Requests a URL, returning a promise.
- * @see http://web.npm.alibaba-inc.com/package/@alipay/ajax
+ *
+ * @param  {string} url       The URL we want to request
+ * @param  {object} [options] The options we want to pass to "fetch"
+ * @return {object}           An object containing either "data" or "err"
  */
-export default function request(url, { method = 'get', ...options } = {}) {
-  const DEFAULTS = {
-    url,
-    type: 'json',
-    method,
-    // 超时时间，单位秒
-    timeout: REQUEST_TIMEOUT,
-    cache: false,
+export default async function request(url, options) {
+  const response = await fetch(url, options);
+
+  checkStatus(response);
+
+  const data = await response.json();
+
+  const ret = {
+    data,
+    headers: {},
   };
 
-  if (method === 'post') {
-    DEFAULTS.contentType = 'application/x-www-form-urlencoded; charset=utf-8';
+  if (response.headers.get('x-total-count')) {
+    ret.headers['x-total-count'] = response.headers.get('x-total-count');
   }
 
-  const reqParams = {
-    ...DEFAULTS,
-    ...options,
-  };
-
-  debug.req(`Request => ${url}`, reqParams);
-
-  return ajax(reqParams).then((data, status, xhr) => { // eslint-disable-line
-    debug.res('Response => %s %o', url, data);
-    localStorageDebug(`Res_form_${url}`, JSON.stringify(data));
-
-    let error = null;
-
-    if (data) {
-      // 登录状态检查
-      checkLoginStatus(data);
-
-      // 错误状态处理
-      error = failedHandler(data);
-
-      return ({ data, error });
-    }
-
-    return ({ data: null, error });
-  }).catch(err => {
-    console.log('AJAX Error', err);
-
-    let code = null;
-    let {
-      message,
-    } = err;
-
-    if (message === 'timeout') {
-      message = '请求超时，请重试！';
-      code = ERROR_CODE.TIMEOUT;
-    } else if (message.indexOf('Unexpected token') > -1) {
-      message = '数据解析出错，请重试！';
-      code = ERROR_CODE.SYNTAX_ERROR;
-    }
-
-    message = message || '请求数据出错，请重试！';
-
-    toast({
-      type: 'error',
-      msg: message,
-    });
-
-    const error = {
-      message,
-      status: 'error',
-      code,
-    };
-
-    return ({ error });
-  });
-};
+  return ret;
+}
