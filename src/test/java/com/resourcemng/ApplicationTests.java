febaa59a6ef26beff7eeedb4d9fe69b5fb7296bc
yhyan594900358 <594900358@qq.com>
2017-06-17 18:23:19 +0800

    添加测试样例

--- src/test/java/com/resourcemng/ApplicationTests.java
+++ src/test/java/com/resourcemng/ApplicationTests.java
@@ -1,18 +1,58 @@
 package com.resourcemng;
 
+import com.resourcemng.entity.User;
+import com.resourcemng.repository.TUserRepository;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+import java.util.List;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+@RunWith(SpringRunner.class)
 @WebAppConfiguration
+@SpringBootTest
 public class ApplicationTests {
+  private MockMvc mvc;
+  @Before
+  public void setUp() throws Exception {
+    mvc = MockMvcBuilders.standaloneSetup(new Application()).build();
+  }
+  //@Test
+  public void getHello() throws Exception {
+    mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andExpect(content().string(equalTo("Hello World!")));
+  }
+  @Autowired
+  private TUserRepository userRepository;
+  @Test
+  public void test() throws Exception {
 
-	@Test
-	public void contextLoads() {
-	}
+    // 创建10条记录
+    User entity = new User();
+    entity.setName("AAAAA");
+    entity.setAge(444);
+    userRepository.save(entity);
 
+    List<User> u=userRepository.findByName("AAAAA");
+    Assert.assertEquals(1, u.size());
+
+  }
 }
