0399dd0815bf5625220fe867752482010abb53f2
yuqingHe <heyuqing@hfdeMacBook-Pro.local>
2017-06-17 10:24:50 +0800

    项目预算页面

---
+++ src/components/budget/ProjectBudgetList.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { connect } from 'dva';
+import { Table, Pagination } from 'antd';
+import { routerRedux } from 'dva/router';
+import FrameContent from '../components/common/FrameContent';
+
+function Users({ dispatch, list: dataSource, loading, total, page: current }) {
+  function pageChangeHandler(page) {
+    dispatch(routerRedux.push({
+      pathname: '/users',
+      query: { page }
+    }));
+  }
+  const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text, { id }) => <a href="" > { text } {id} </a>,
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+  {
+    title: 'Website',
+    dataIndex: 'website',
+    key: 'website',
+  },
+  {
+    title: 'Operation',
+    key: 'operation',
+  },
+  ];
+
+  return (
+    <FrameContent>
+      <div>
+        <Table
+          columns={columns}
+          dataSource={dataSource}
+          loading={loading}
+          rowKey={record => record.id}
+          pagination={false}
+        />
+        <Pagination
+          className="ant-table-pagination"
+          total={total}
+          current={current}
+          pageSize={20}
+          onChange={pageChangeHandler}
+        />
+      </div>
+    </FrameContent>
+  );
+}
+function mapStateToProps(state) {
+  const { list, total, page } = state.listmodel;
+  return {
+    loading: state.loading.models.users,
+    list,
+    total,
+    page,
+  };
+}
+
+export default connect(mapStateToProps)(Users);
