189a0e24072160a0adabdf4921da8d69ea650e4c
yhyan594900358 <594900358@qq.com>
2017-06-27 20:18:14 +0800

    绩效目标导入实现

---
+++ src/main/java/com/resourcemng/Enum/FoundSourceType.java
@@ -0,0 +1,15 @@
+package com.resourcemng.Enum;
+
+/**
+ * 审核类型
+ * Created by 燕子 on 2017/6/20.
+ */
+public class FoundSourceType {
+
+  public static final String TOTAL = "0";
+  public static final String COUNTRY = "1";
+  public static final String LOCAL = "2";
+  public static final String ENTERPRICE = "3";
+  public static final String UNIVERSITY = "4";
+
+}
