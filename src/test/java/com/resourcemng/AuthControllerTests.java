8ee18a29a1b7fb970512a4422d0c478bdc37b5f8
yhyan594900358 <594900358@qq.com>
2017-07-01 09:19:07 +0800

    绩效目标导入实现

---
+++ src/test/java/com/resourcemng/AuthControllerTests.java
@@ -0,0 +1,38 @@
+package com.resourcemng;
+
+import com.resourcemng.controller.AuthController;
+import com.resourcemng.controller.BudgetController;
+import com.resourcemng.entitys.Tuser;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@SpringBootTest
+public class AuthControllerTests {
+  private MockMvc mvc;
+  @Before
+  public void setUp() throws Exception {
+    mvc = MockMvcBuilders.standaloneSetup(new Application()).build();
+  }
+  //@Test
+  public void getHello() throws Exception {
+  }
+  @Autowired
+  private AuthController authController;
+  @Test
+  public void test() throws Exception {
+    Tuser tuser = new Tuser();
+    tuser.setUsername("2016-02-2");
+    tuser.setPassword("123456");
+    Object obj = authController.loginPost(tuser);
+  }
+}
