2b4f2ad07fff46652bf2689ded718a1cbc80e1de
yhyan594900358 <594900358@qq.com>
2017-07-03 07:48:24 +0800

    调试

--- src/main/java/com/resourcemng/Enum/ForViewAuditStatus.java
+++ src/main/java/com/resourcemng/Enum/ForViewAuditStatus.java
@@ -4,9 +4,17 @@ package com.resourcemng.Enum;
  * Created by 燕子 on 2017/6/20.
  */
 public class ForViewAuditStatus {
-
+  /**
+   * 没有审核
+   */
   public static String UN_AUDIT = "0";
+  /**
+   * 财务已经审核
+   */
   public static String FINACE_AUDIT = "1";
+  /**
+   *
+   */
   public static String SCHOOL_AUDIT = "2";
   public static String CONUTTRY_AUDIT = "3";
 
