4111852da6e61ae5036e90faaca509e95795e5e7
yhyan594900358 <594900358@qq.com>
2017-07-09 10:13:30 +0800

    调试

---
+++ sql/initdata.sql
@@ -0,0 +1,58 @@
+
+-- ----------------------------
+-- Records of tuser
+-- ----------------------------
+
+
+INSERT INTO `tuser` VALUES ('3', 'admin', '专业', '1Rm86eLaMmD7TWhKUiXRXg==', '1', NULL, '0', '0514-87433080', NULL);
+INSERT INTO `tuser` VALUES ('451', 'cws666', '无', 'fx3inm2hnSK1HGgAHn4OVA==', '6', NULL, '0', '15964075689', NULL);
+
+-- ----------------------------
+-- Records of report_deadline_setting
+-- ----------------------------
+INSERT INTO `report_deadline_setting` VALUES ('quarter-four', '16', '第四季度1月份上报截止日期');
+INSERT INTO `report_deadline_setting` VALUES ('quarter-one', '12', '第一季度4月份上报截止日期');
+INSERT INTO `report_deadline_setting` VALUES ('quarter-three', '15', '第三季度10月份上报截止日期');
+INSERT INTO `report_deadline_setting` VALUES ('quarter-two', '13', '第二季度7月份上报截止日期');
+
+
+-- ----------------------------
+-- Records of commoncode
+-- ----------------------------
+INSERT INTO `commoncode` VALUES ('北京市', null, '1', 'provence', 'provence1');
+INSERT INTO `commoncode` VALUES ('江苏省', null, '10', 'provence', 'provence10');
+INSERT INTO `commoncode` VALUES ('浙江省', null, '11', 'provence', 'provence11');
+INSERT INTO `commoncode` VALUES ('安徽省', null, '12', 'provence', 'provence12');
+INSERT INTO `commoncode` VALUES ('福建省', null, '13', 'provence', 'provence13');
+INSERT INTO `commoncode` VALUES ('江西省', null, '14', 'provence', 'provence14');
+INSERT INTO `commoncode` VALUES ('山东省', null, '15', 'provence', 'provence15');
+INSERT INTO `commoncode` VALUES ('河南省', null, '16', 'provence', 'provence16');
+INSERT INTO `commoncode` VALUES ('湖北省', null, '17', 'provence', 'provence17');
+INSERT INTO `commoncode` VALUES ('湖南省', null, '18', 'provence', 'provence18');
+INSERT INTO `commoncode` VALUES ('广东省', null, '19', 'provence', 'provence19');
+INSERT INTO `commoncode` VALUES ('天津市', null, '2', 'provence', 'provence2');
+INSERT INTO `commoncode` VALUES ('广西省', null, '20', 'provence', 'provence20');
+INSERT INTO `commoncode` VALUES ('海南省', null, '21', 'provence', 'provence21');
+INSERT INTO `commoncode` VALUES ('重庆市', null, '22', 'provence', 'provence22');
+INSERT INTO `commoncode` VALUES ('四川省', null, '23', 'provence', 'provence23');
+INSERT INTO `commoncode` VALUES ('贵州省', null, '24', 'provence', 'provence24');
+INSERT INTO `commoncode` VALUES ('云南省', null, '25', 'provence', 'provence25');
+INSERT INTO `commoncode` VALUES ('西藏', null, '26', 'provence', 'provence26');
+INSERT INTO `commoncode` VALUES ('陕西省', null, '27', 'provence', 'provence27');
+INSERT INTO `commoncode` VALUES ('甘肃省', null, '28', 'provence', 'provence28');
+INSERT INTO `commoncode` VALUES ('青海省', null, '29', 'provence', 'provence29');
+INSERT INTO `commoncode` VALUES ('河北省', null, '3', 'provence', 'provence3');
+INSERT INTO `commoncode` VALUES ('宁夏市', null, '30', 'provence', 'provence30');
+INSERT INTO `commoncode` VALUES ('新疆', null, '31', 'provence', 'provence31');
+INSERT INTO `commoncode` VALUES ('山西省', null, '4', 'provence', 'provence4');
+INSERT INTO `commoncode` VALUES ('内蒙古', null, '5', 'provence', 'provence5');
+INSERT INTO `commoncode` VALUES ('辽宁省', null, '6', 'provence', 'provence6');
+INSERT INTO `commoncode` VALUES ('吉林省', null, '7', 'provence', 'provence7');
+INSERT INTO `commoncode` VALUES ('黑龙江省', null, '8', 'provence', 'provence8');
+INSERT INTO `commoncode` VALUES ('上海市', null, '9', 'provence', 'provence9');
+
+
+
+
+
+
